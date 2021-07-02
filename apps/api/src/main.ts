cdba1fbc6d43526b7831bdea51ad279108dfd20c
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-07-02 12:14:32 +0300

    todos:

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,16 +3,19 @@
  * This is only a minimal backend to get started.
  */
 
-import * as express from 'express';
+import * as express from "express";
+import { addTodoRoutes } from "./app/todo";
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
+app.get("/api", (req, res) => {
+  res.send({ message: "Welcome to api!" });
 });
+
+addTodoRoutes(app);
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
-server.on('error', console.error);
+server.on("error", console.error);
