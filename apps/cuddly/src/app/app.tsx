ea82c5967bd6fe164f95e9dfbfede940b02e06b7
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-07-04 18:59:16 +0300

    update

--- apps/cuddly/src/app/app.tsx
+++ apps/cuddly/src/app/app.tsx
@@ -1,40 +1,12 @@
 import React from 'react';
-
-interface Todo {
-  title: string;
-}
+import CartContainer from '../components/comon/cart/cartContainer';
+import ShelfContainer from '../components/comon/shelf/shelfContainer';
 
 function App() {
-  const [todos, setTodos] = React.useState<Todo[]>([]);
-
-  const addTodo = () => {
-    fetch('/api/addTodo', {
-      method: 'POST',
-      body: '',
-    })
-      .then((_) => _.json())
-      .then((newTodo) => {
-        setTodos([...todos, newTodo]);
-      });
-  };
-
-  React.useEffect(() => {
-    fetch('/api/todos')
-      .then((_) => _.json())
-      .then(setTodos);
-  }, []);
-
   return (
-    <div>
-      <h1>Todos</h1>
-      <ul>
-        {todos.map((t) => (
-          <li className={'todo'}>{t.title}</li>
-        ))}
-      </ul>
-      <button id={'add-todo'} onClick={addTodo}>
-        Add Todo
-      </button>
+    <div className="App">
+      <CartContainer />
+      <ShelfContainer />
     </div>
   );
 }
