de65e8d9b7a3b2357b20782ed4a61b301c5316c5
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-07-07 10:08:59 +0300

    apps/api/src/app/cart/cart.routes.config:

--- apps/api/src/app/cart/cart.routes.config.ts
+++ apps/api/src/app/cart/cart.routes.config.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import { CommonRoutesConfig } from "../../common/common.routes.config";
+import cartControllers from "./controllers/cart.controllers";
 
 export class CartRoutes extends CommonRoutesConfig {
   constructor(app: express.Application) {
@@ -7,12 +8,7 @@ export class CartRoutes extends CommonRoutesConfig {
   }
 
   configureRoutes() {
-    this.app
-      .route(`/cart/:id`)
-      .get((req: express.Request, res: express.Response) => {
-        res.status(200).send({ message: "Cart" });
-      })
-      .post();
+    this.app.route(`/cart/:id`).get(cartControllers.getCartItems).post();
 
     this.app.route(`/cart/:userId/:itemId`);
 
