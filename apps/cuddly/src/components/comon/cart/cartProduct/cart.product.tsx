d23e8009f05132ade3eb38cc8c8f77a8d1be4eec
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-07-08 22:12:45 +0300

    cart.product: toggleMouseOverState

--- apps/cuddly/src/components/comon/cart/cartProduct/cart.product.tsx
+++ apps/cuddly/src/components/comon/cart/cartProduct/cart.product.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import { CartProductProps } from "./cart.product.container";
+import * as React from 'react';
+import { CartProductProps } from './cart.product.container';
 
 interface State {
   isOverRemoveIcon: boolean;
@@ -14,31 +14,44 @@ class CartProductView extends React.Component<CartProductProps, State> {
     const { product } = this.props;
 
     const { isOverRemoveIcon } = this.state;
-    const descCss = isOverRemoveIcon ? "desc desc--remove" : "desc";
+    const descCss = isOverRemoveIcon ? 'desc desc--remove' : 'desc';
 
     return (
-      <div className='card-item'>
-        <div className='remove-icon'></div>
-        <div className='thumb'>
+      <div className="card-item">
+        <div
+          className="remove-icon"
+          onMouseEnter={this.toggleMouseOverState}
+          onMouseLeave={this.toggleMouseOverState}
+          onClick={() => this.props.removeCartProduct(product, product.sku)}
+        />
+        <div className="thumb">
           <img
-            src='https://images.unsplash.com/photo-1521330784804-5f69f8a17b1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80'
+            src="https://images.unsplash.com/photo-1521330784804-5f69f8a17b1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"
             alt={product.title}
             title={product.title}
           />
         </div>
-        <div className='details'>
-          <p className='title'>{product.title}</p>
+        <div className="details">
+          <p className="title">{product.title}</p>
           <p className={descCss}>
             {`${product.availableSizes[0]} | ${product.style}`} <br />
             Quantity: {product.itemQuantity}
           </p>
         </div>
-        <div className='price-css'>
+        <div className="price-css">
           <p>{`${product.currencyFormat} ${product.price}`}</p>
         </div>
       </div>
     );
   }
+
+  private toggleMouseOverState = () => {
+    this.setState((prev) => {
+      this.setState({
+        isOverRemoveIcon: !prev.isOverRemoveIcon,
+      });
+    });
+  };
 }
 
 export default CartProductView;
