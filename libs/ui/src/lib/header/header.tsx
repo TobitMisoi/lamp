edceb55e3d97c376efd1571405eb554d2e6f4373
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-07-12 21:47:43 +0300

    mods

--- libs/ui/src/lib/header/header.tsx
+++ libs/ui/src/lib/header/header.tsx
@@ -1,7 +1,22 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Mousewheel, Pagination } from 'swiper/core';
+import SwiperCore from 'swiper';
+
+import 'swiper/swiper.min.css';
+import 'swiper/components/pagination/pagination.min.css';
 /* eslint-disable-next-line */
 export interface HeaderProps {}
 
+SwiperCore.use([Mousewheel, Pagination]);
+
 export function Header(props: HeaderProps) {
+  const pagination = {
+    clickable: true,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    renderBullet: function (index: any, className: string) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  };
   return (
     <header>
       <nav className="header">
@@ -23,19 +38,45 @@ export function Header(props: HeaderProps) {
         </div>
       </nav>
       <article>
-        <div className="content">
-          <div className="content-detail">
-            <h3>New Arrival</h3>
-            <h1>Cat Tee</h1>
-            <button>Start Shopping</button>
-          </div>
-          <div className="content-image">
-            <img
-              src="https://images.unsplash.com/photo-1606522754091-a3bbf9ad4cb3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1067&q=80"
-              alt=""
-            />
-          </div>
-        </div>
+        <Swiper
+          direction={'vertical'}
+          slidesPerView={1}
+          spaceBetween={30}
+          mousewheel={true}
+          pagination={pagination}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div className="content">
+              <div className="content-detail">
+                <h3>New Arrival</h3>
+                <h1>Cat Tee</h1>
+                <button>Start Shopping</button>
+              </div>
+              <div className="content-image">
+                <img
+                  src="https://images.unsplash.com/photo-1606522754091-a3bbf9ad4cb3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1067&q=80"
+                  alt=""
+                />
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="content">
+              <div className="content-detail">
+                <h3>New Arrival</h3>
+                <h1>Van Prt</h1>
+                <button>Start Shopping</button>
+              </div>
+              <div className="content-image">
+                <img
+                  src="https://images.unsplash.com/photo-1606522754091-a3bbf9ad4cb3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1067&q=80"
+                  alt=""
+                />
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </article>
     </header>
   );
